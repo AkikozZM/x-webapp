f22cf55dc280879274eeee1d5eca591f387b7eb4
AkikozZM <zgeming@seas.upenn.edu>
2025-05-30 15:44:27 -0700

    leftsidebar userbar

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { NAVIGATION_ITEMS } from "@/constants/icons";
 import { BsThreeDots } from "react-icons/bs";
+import { createClient } from "../../utils/supabase/client";
+
+interface User {
+  user_metadata?: {
+    username?: string;
+  };
+  email?: string;
+}
 
 const LeftSidebar = () => {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    // Simulate fetching user data
+    const fetchUser = async () => {
+      const supabase = createClient();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <section className="fixed w-66 flex-col flex items-stretch h-screen">
       {/* Left Nav bar */}
@@ -27,10 +49,16 @@ const LeftSidebar = () => {
       {/* User Avatar */}
       <button className="w-full rounded-full flex items-center justify-between py-5 px-1 text-black  hover:bg-black/10 cursor-pointer transition duration-200">
         <div className="items-center justify-center flex space-x-2">
-          <div className="rounded-full bg-slate-400 w-10 h-10">A </div>
+          <div className="rounded-full bg-slate-400 w-10 h-10">
+            <div className="text-black text-lg flex items-center justify-center h-full">
+              {user?.user_metadata?.username?.charAt(0).toUpperCase() || ""}
+            </div>
+          </div>
           <div className="text-left text-sm">
-            <div className="font-bold">User</div>
-            <div>@UserName</div>
+            <div className="font-bold">
+              {user?.user_metadata?.username || "User"}
+            </div>
+            <div>@{user?.user_metadata?.username || "username"}</div>
           </div>
         </div>
         <div>
